3d47a12f5132eac35ffee05f13b748c46bb10024
Anam Qureshi <anamqureshi@Anams-MacBook-Pro.local>
2024-04-22 15:30:46 -0400

    activities

--- data/activities.js
+++ data/activities.js
@@ -1,7 +1,9 @@
 import { ObjectId } from mongodb;
+import { activities, reviews } from "../config/mongoCollections.js";
+import { dbConnection } from "../config/mongoConnection.js";
+import Activity from "../models/activities.js";
 
 const createActivity = async (
-    activityId,
     activityName,
     activityAddress,
     activityHours,
@@ -13,73 +15,76 @@ const createActivity = async (
     activityLink,
     activityDescription
 ) => {
+    
+    const activityCollection = await activities();
 
+    const newActivity = {
+        activityName,
+        activityAddress,
+        activityHours,
+        activityType,
+        rating,
+        activityAccommodations,
+        activityPriceRange,
+        reviews,
+        activityLink,
+        activityDescription
+      };
+
+      const insertInfo = await activityCollection.insertOne(newActivity);
+      if (insertInfo.insertedCount === 0) throw new Error("Could not add activity");
+    
+      return newActivity;
+    };
+
+
+
+const getAllActivities = async () => {
+    const activityCollection = await activities();
+    const activityList = await activityCollection.find({}).toArray();
+    return activityList;
+};
+
+const getActivity = async (activityId) => {
+    if (!activityId || typeof activityId !== 'string' || !ObjectId.isValid(activityId))
+    throw new Error("Invalid activity ID");
+
+    const activityCollection = await activities();
+    const activity = await activityCollection.findOne({ _id: ObjectId(activityId) });
+    if (!activity) throw new Error("Activity not found");
+
+    return activity;
 };
 
 
-const getAllActivities = async (
-    activityId,
-    activityName,
-    activityAddress,
-    activityHours,
-    activityType,
-    rating,
-    activityAccommodations,
-    activityPriceRange,
-    reviews,
-    activityLink,
-    activityDescription
-) => {
+const removeActivity = async (activityId) => {
+    if (!activityId || typeof activityId !== 'string' || !ObjectId.isValid(activityId))
+    throw new Error("Invalid activity ID");
 
-};
+    const activityCollection = await activities();
+    const deletionInfo = await activityCollection.deleteOne({ _id: ObjectId(activityId) });
+    if (deletionInfo.deletedCount === 0) throw new Error("Could not delete activity");
 
-const getActivityById = async (
-    activityId,
-    activityName,
-    activityAddress,
-    activityHours,
-    activityType,
-    rating,
-    activityAccommodations,
-    activityPriceRange,
-    reviews,
-    activityLink,
-    activityDescription
-) => {
-
-};
-
-
-const removeActivityById = async (
-    activityId,
-    activityName,
-    activityAddress,
-    activityHours,
-    activityType,
-    rating,
-    activityAccommodations,
-    activityPriceRange,
-    reviews,
-    activityLink,
-    activityDescription
-) => {
-
+    return true;
 };
 
 const updateActivity = async (
     activityId,
-    activityName,
-    activityAddress,
-    activityHours,
-    activityType,
-    rating,
-    activityAccommodations,
-    activityPriceRange,
-    reviews,
-    activityLink,
-    activityDescription
+    updatedActivityData
 ) => {
+    if (!activityId || typeof activityId !== 'string' || !ObjectId.isValid(activityId))
+    throw new Error("Invalid activity ID");
 
+    const activityCollection = await activities();
+
+    const updatedActivity = {
+        $set: updatedActivityData
+  };
+
+    const updateInfo = await activityCollection.updateOne({ _id: ObjectId(activityId) }, updatedActivity);
+    if (updateInfo.modifiedCount === 0) throw new Error("Could not update activity");
+
+    return true;
 };
 
-
+export { createActivity, getAllActivities, getActivity, removeActivity, updateActivity };
