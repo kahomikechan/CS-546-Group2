c65723797f972ae4452da89afac091ecda665595
Anam Qureshi <anamqureshi@Anams-MacBook-Pro.local>
2024-04-25 15:48:19 -0400

    updated routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 import userRoutes from './users.js';
 import activitiesRouter from './activities.js';
 import eventsRouter from './events.js';
+import reviewsRouter from './reviews.js';
 
 const constructorMethod = (app) => {
   app.use('/', userRoutes);
   app.use('/', activitiesRouter);
   app.use('/', eventsRouter);
+  app.use('/', reviewsRouter);
 
   app.use('*', (req, res) => {
     return res.status(404).json({error: 'Not found'});
