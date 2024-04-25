330457000f7ebf99b2350c6bcf6752fd2b277924
Anam Qureshi <anamqureshi@Anams-MacBook-Pro.local>
2024-04-25 15:46:39 -0400

    user access

--- routes/events.js
+++ routes/events.js
@@ -3,9 +3,28 @@ import { createEvent, getAllEvents, getEvent, removeEvent, updateEvent } from '.
 
 const eventsRouter = express.Router();
 
+//checks if the user is an admin
+const isAdmin = (req, res, next) => {
+  if (req.session.user && req.session.user.role === 'admin') {
+    next();
+  } else {
+    res.status(403).json({ error: 'Access forbidden' });
+  }
+};
+
+//checks if the user is logged in/approved by admin
+const isAuthenticated = (req, res, next) => {
+  if (req.session.user) {
+    next();
+  } else {
+    res.status(401).json({ error: 'Unauthorized' });
+  }
+};
+
+
 // /event
 // route to create a new event - logged in user
-eventsRouter.post('/createEvent', async (req, res) => {
+eventsRouter.post('/createEvent', isAuthenticated, async (req, res) => {
   try {
     const eventData = req.body; 
     const newEvent = await createEvent(eventData);
@@ -16,7 +35,7 @@ eventsRouter.post('/createEvent', async (req, res) => {
 });
 
 // route to get all events - logged in user
-eventsRouter.get('/allEvents', async (req, res) => {
+eventsRouter.get('/allEvents', isAuthenticated, async (req, res) => {
   try {
     const allEvents = await getAllEvents();
     res.json(allEvents);
@@ -26,7 +45,7 @@ eventsRouter.get('/allEvents', async (req, res) => {
 });
 
 // route to get a specific event by ID - logged in user
-eventsRouter.get('/event/:id', async (req, res) => {
+eventsRouter.get('/event/:id',isAuthenticated, async (req, res) => {
   try {
     const eventId = req.params.id;
     const event = await getEvent(eventId);
@@ -42,7 +61,7 @@ eventsRouter.get('/event/:id', async (req, res) => {
 
 // make this so only admin can do it
 // route to update an existing event - only admin 
-eventsRouter.put('/updateEvent/:id', async (req, res) => {
+eventsRouter.put('/updateEvent/:id', isAdmin, async (req, res) => {
   try {
     const eventId = req.params.id;
     const updatedEventData = req.body; 
