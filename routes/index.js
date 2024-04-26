11f905e5bd9a79282b8903d5565db1ecb6648a66
Anam Qureshi <anamqureshi@Anams-MacBook-Pro.local>
2024-04-26 11:50:00 -0400

    added searchrouter to consmethod

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ import userRoutes from './users.js';
 import activitiesRouter from './activities.js';
 import eventsRouter from './events.js';
 import reviewsRouter from './reviews.js';
+import searchRouter from './search.js';
 
 const constructorMethod = (app) => {
   app.use('/', userRoutes);
   app.use('/', activitiesRouter);
   app.use('/', eventsRouter);
   app.use('/', reviewsRouter);
+  app.use ('/', searchRouter);
 
   app.use('*', (req, res) => {
     return res.status(404).json({error: 'Not found'});
