2c7c0780aeb99b164f3dac09b83a5645d5b80ac4
kahomikechan <161900037+kahomikechan@users.noreply.github.com>
2024-04-28 18:09:56 -0400

    Delete views/config directory

--- views/config/settings.js
+++
@@ -1,6 +0,0 @@
-// You will need to change the DB name to match the required DB name in the assignment specs!
-export const mongoConfig = {
-    serverUrl: 'mongodb://localhost:27017/',
-    database: 'CS546_Group2_Final_Project'
-  };
-  
